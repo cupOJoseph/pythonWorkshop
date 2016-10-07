9321c3276fab67755146cc4288ea9f5a9f017559
Joseph Schiarizzi <EVE@Josephs-MacBook-Pro.local>
2016-10-06 21:09:55 -0400

    added comments and walkthrough for hello world program

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,1 +1,30 @@
+'''
+Create a hello world program.
+Manipute a string a little bit.
+Notice that this is a comment, it doesn't effect the code.
+I can comment here because of the three ' s
+'''
 
+s = "Hello World"
+
+'''
+This is a comment that tells us about the following function.
+Good documentation is important
+This function uses the string s, which we expect to be "Hello World"
+to do stuff and things.
+@param - None
+@return - None
+'''
+def learnPython():
+	#This is just a single line comment
+	#print("Running hello world program")
+	x = 0
+	if(1){
+
+	}elif(0){
+
+	}else{
+
+	}
+
+learnPython() #this tells the program to run learnPython function
