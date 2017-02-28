e6937b30d590dcd1ba729520ae75074608e083c0
jschiarizzi <jschiarizzi@gwu.edu>
2017-02-28 17:31:29 -0500

    removed completion from files for new workshop

--- LittleHighLittleLow.py
+++ LittleHighLittleLow.py
@@ -6,27 +6,29 @@ Sort the list
 Decide and print the largest and the smallest number
 '''
 
+#setup data structures
 times = input("Enter a number: ")
 times = int(times)
 numList = {}
 
+#get a list of numbers from the user
 for i in range(times):
 	#what goes here? hint: INPUT from user
-	print("Enter the", i, "th number, between 0 and 100")
-	num = input("")
-	num = int(num)
-	numList[i] = num
 
+
+print("Printing out numbers in list:")
 print(numList)
 
 minVal = 101
 maxVal = -1
 
+
+#decide what the smallest and largest numbers are
 for j in numList:
 	#what do we want to check here?
-	if(numList[j] < minVal):
+	if(        ):
 		minVal = numList[j] #numList[j] becomes each element in the list
-	elif(numList[j] > maxVal):
+	elif(      ):
 		maxVal = numList[j]
 
 
